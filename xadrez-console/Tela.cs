b0454922e3a7d2535f5d262aa3c9d2e0b0c5c3ef
Luciano Bessegato <lfrbessegato@gmail.com>
2019-10-24 23:03:15 -0300

    Tratando erros com excecao personalizada e correcao da Tela

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -14,7 +14,7 @@ namespace xadrez_console
                 {
                     if (tab.peca(i,j) == null)
                     {
-                        Console.Write(" -");
+                        Console.Write("- ");
                     }
                     else
                     {
